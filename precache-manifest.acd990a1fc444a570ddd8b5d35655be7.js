bc6434e67fdfec92b1b8286e82766d13e769da6a
Daniel Garcia <danielg_97@yahoo.com>
2021-02-09 03:28:36 -0500

    Updates

---
+++ precache-manifest.acd990a1fc444a570ddd8b5d35655be7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d51f220f7ebf886927b0ee05ac8c39",
+    "url": "/c4me-College-4-Me/index.html"
+  },
+  {
+    "revision": "3c0f42c98e47e9d84310",
+    "url": "/c4me-College-4-Me/static/css/main.0eb5ea94.chunk.css"
+  },
+  {
+    "revision": "a5004e3eff5265de0069",
+    "url": "/c4me-College-4-Me/static/js/2.a8f42b60.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/c4me-College-4-Me/static/js/2.a8f42b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0f42c98e47e9d84310",
+    "url": "/c4me-College-4-Me/static/js/main.309168fb.chunk.js"
+  },
+  {
+    "revision": "3325b59b78f351ba32eb",
+    "url": "/c4me-College-4-Me/static/js/runtime-main.0e1d47a5.js"
+  },
+  {
+    "revision": "0581e02788013cdfe4ba0076e17c37a8",
+    "url": "/c4me-College-4-Me/static/media/Stony Brook University.0581e027.jpg"
+  },
+  {
+    "revision": "32a1ef63519115446155b74aef80bb1d",
+    "url": "/c4me-College-4-Me/static/media/background.32a1ef63.jpg"
+  },
+  {
+    "revision": "9947e0f84733874721a15270f6d43ee0",
+    "url": "/c4me-College-4-Me/static/media/editbutton.9947e0f8.png"
+  },
+  {
+    "revision": "c3016a626b7c0ca40a7c8567dab9f539",
+    "url": "/c4me-College-4-Me/static/media/logo.c3016a62.png"
+  }
+]);
