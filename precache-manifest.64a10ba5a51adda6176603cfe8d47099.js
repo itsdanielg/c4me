d73e30d287ee1c03d402cff5f5260fd360d39a68
Daniel Garcia <danielg_97@yahoo.com>
2021-02-09 03:54:36 -0500

    Updates

---
+++ precache-manifest.64a10ba5a51adda6176603cfe8d47099.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0135177d0714450964f59848cccb5b",
+    "url": "/c4me/index.html"
+  },
+  {
+    "revision": "6967d55636df97966987",
+    "url": "/c4me/static/css/main.2f5e9355.chunk.css"
+  },
+  {
+    "revision": "a5004e3eff5265de0069",
+    "url": "/c4me/static/js/2.a8f42b60.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/c4me/static/js/2.a8f42b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6967d55636df97966987",
+    "url": "/c4me/static/js/main.309168fb.chunk.js"
+  },
+  {
+    "revision": "3c93f0950edb78c4a38a",
+    "url": "/c4me/static/js/runtime-main.18f5d66f.js"
+  },
+  {
+    "revision": "0581e02788013cdfe4ba0076e17c37a8",
+    "url": "/c4me/static/media/Stony Brook University.0581e027.jpg"
+  },
+  {
+    "revision": "32a1ef63519115446155b74aef80bb1d",
+    "url": "/c4me/static/media/background.32a1ef63.jpg"
+  },
+  {
+    "revision": "9947e0f84733874721a15270f6d43ee0",
+    "url": "/c4me/static/media/editbutton.9947e0f8.png"
+  },
+  {
+    "revision": "c3016a626b7c0ca40a7c8567dab9f539",
+    "url": "/c4me/static/media/logo.c3016a62.png"
+  }
+]);
